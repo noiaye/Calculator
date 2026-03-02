e206e513258e87fa767ea54f7691849f3498844a
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-03-02 07:14:46 +0100

    Add default structure and functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculator
+
+In this project, I will be combining everything i have learned so far, into one project: The calculator.
---
+++ javascript.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(add(1, 2))
+console.log(subtract(4, 1))
+console.log(divide(6, 2))
+console.log(multiply(3, 1))
